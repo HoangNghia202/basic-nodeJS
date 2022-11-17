7134bd311b5fb78d4d7724744b293871d216f5e3
Hoang Nghia <hoangnghianguyendac@gmail.com>
2022-11-17 20:44:36 +0700

    create new user, display all users

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,9 +1,10 @@
 import db from "../models/index.js";
+import CRUDService from "../services/CRUDService.js";
 let getHomePage = async (req, res) => {
   try {
     let data = await db.User.findAll();
     console.log(">>>getHomePage");
-    console.log(">>>data", data);
+    // console.log(">>>data", data);
     return res.render("homePage.ejs", { userData: JSON.stringify(data) });
   } catch (err) {
     console.log(err);
@@ -15,7 +16,29 @@ let getAboutPage = (req, res) => {
   return res.render("aboutPage.ejs");
 };
 
+let getCRUD = (req, res) => {
+  console.log(">>>getCRUD");
+  return res.render("crud.ejs");
+};
+
+let postCRUD = async (req, res) => {
+  console.log(">>>postCRUD method");
+  let message = await CRUDService.createNewUser(req.body);
+  console.log("", message);
+  return res.send("postCRUD form server");
+};
+
+let displayGetCRUD = async (req, res) => {
+  let users = await CRUDService.getAllUser();
+  console.log("", users);
+
+  return res.render("displayCrud.ejs", { users: users });
+};
+
 module.exports = {
   getHomePage: getHomePage,
   getAboutPage: getAboutPage,
+  getCRUD: getCRUD,
+  postCRUD: postCRUD,
+  displayGetCRUD: displayGetCRUD,
 };
